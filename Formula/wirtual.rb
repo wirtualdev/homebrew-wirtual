55a97bd1af73f848b7b7d6040015b0009aa0240b
Wirtual CI <ci@wirtual.dev>
2026-01-13 00:49:01 +0530

    wirtual 5.1.1

--- Formula/wirtual.rb
+++ Formula/wirtual.rb
@@ -4,27 +4,27 @@
 class Wirtual < Formula
   desc "A self-hosted remote development platform"
   homepage "https://wirtual.dev"
-  version "5.1.0"
+  version "5.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wirtualdev/wirtualdev/releases/download/v5.1.0/wirtual_5.1.0_darwin_arm64.zip"
-      sha256 "a189f67ae2ddc20d2ac3c50e194c43e9eee61b79b537e2d9738ece4282a6122a"
+      url "https://github.com/latticeHQ/lattice-runtime/releases/download/v5.1.1/wirtual_5.1.1_darwin_arm64.zip"
+      sha256 "9cd53be8e6af7bfab1d63781448c0f4fed54ab54300d7dd73994019eb20f4b63"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/wirtualdev/wirtualdev/releases/download/v5.1.0/wirtual_5.1.0_darwin_amd64.zip"
-      sha256 "73a29af06e150bf7ac29d008d6924ce05b36de10ae8e6c98c6d57eb7793fa43b"
+      url "https://github.com/latticeHQ/lattice-runtime/releases/download/v5.1.1/wirtual_5.1.1_darwin_amd64.zip"
+      sha256 "3e047daed6d1d4a1c6b9309d386ec9eb337eb4e669af4e78770846438fa44bae"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/wirtualdev/wirtualdev/releases/download/v5.1.0/wirtual_5.1.0_linux_arm64.tar.gz"
-      sha256 "cf44d87cef721f21072403972b1ac79abdc7644218a377553ffaf2db5170955f"
+      url "https://github.com/latticeHQ/lattice-runtime/releases/download/v5.1.1/wirtual_5.1.1_linux_arm64.tar.gz"
+      sha256 "ecffb0ce700ef9e49e7c89c50ec0d5fe297b7e324205c2420953dba9a9383892"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/wirtualdev/wirtualdev/releases/download/v5.1.0/wirtual_5.1.0_linux_amd64.tar.gz"
-      sha256 "1a27e71cec26257ed4fed92d742d6b751472821456aa5cb207ef06b83b8778e1"
+      url "https://github.com/latticeHQ/lattice-runtime/releases/download/v5.1.1/wirtual_5.1.1_linux_amd64.tar.gz"
+      sha256 "e849f58843fe328d8b9b579d9f85645f97512736dd4a782ba90ec8e6fc9a4f7e"
     end
   end
 
